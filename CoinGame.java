fbe0d150b308efff1a397a1a3c5166bb504c6b10
s1310034 <s1310034@u-aizu.ac.jp>
2024-07-02 11:34:25 +0900

    HoT: initial revision

---
+++ CoinGame.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+class CoinGame {
+    private String[] strarr = new String[5];
+
+    CoinGame() {
+        for (int i = 0; i < 5; i++) {
+            Random rnd = new Random();
+            if (rnd.nextInt(2) == 0) {
+                strarr[i] = "Heads";
+            } else {
+                strarr[i] = "Tails";
+            }
+        }
+    }
+
+    void run() {
+        int cntH = 0, cntT = 0;
+        System.out.println("Tossing a coin...");
+        for (int i = 1; i <= 3; i++) {
+            Random random = new Random();
+            int rndnum = random.nextInt(5);
+            System.out.println("Round " + i + ": " + strarr[rndnum]);
+            if (strarr[rndnum].equals("Heads")) {
+                cntH++;
+            } else {
+                cntT++;
+            }
+        }
+        System.out.println("Heads: " + cntH + ", Tails: " + cntT);
+    }
+
+    public static void main(String[] args) {
+        new CoinGame().run();
+    }
+}
