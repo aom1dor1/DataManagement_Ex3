99afb8e187c6c3ac0eaaf46822547bd8c51144d0
s1310034 <s1310034@u-aizu.ac.jp>
2024-07-02 11:39:04 +0900

    User name added

--- CoinGame.java
+++ CoinGame.java
@@ -1,7 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class CoinGame {
     private String[] strarr = new String[5];
+    private String name;
 
     CoinGame() {
         for (int i = 0; i < 5; i++) {
@@ -15,6 +17,7 @@ class CoinGame {
     }
 
     void run() {
+        enterName();
         int cntH = 0, cntT = 0;
         System.out.println("Tossing a coin...");
         for (int i = 1; i <= 3; i++) {
@@ -28,6 +31,13 @@ class CoinGame {
             }
         }
         System.out.println("Heads: " + cntH + ", Tails: " + cntT);
+    }
+
+    void enterName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Who are you?");
+        name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 
     public static void main(String[] args) {
